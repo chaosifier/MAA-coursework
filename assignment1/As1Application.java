1a06a7758628aa8428a1dfd06068605818b8278e
Sagar Dahal <=>
2024-05-29 14:41:12 -0500

    completed assignment1

---
+++ assignment1/As1Application.java
@@ -0,0 +1,19 @@
+package com.maa.as1.as1;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class As1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(As1Application.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
+}
